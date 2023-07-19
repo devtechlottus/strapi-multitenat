de6967a92bfe900a5b7deec7c9b33e8fedf6bb35
alan llamas <alanllamasg@gmail.com>
2023-07-19 05:51:14 -0500

    feat: added envioronments

---
+++ config/env/UTEG/plugins.js
@@ -0,0 +1,25 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: `${env('AWS_BUCKET')}/${env('AWS_FOLDER')}`,
+        },
+      },
+    },
+  },
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+      teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
+      roles: ["strapi-super-admin"],
+    },
+  },
+});
